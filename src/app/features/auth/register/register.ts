a11055e7158d8358a9d93ba42d04486977f1d27d
Saptashwa Paul <saptashwapaul5959@gmail.com>
2026-03-29 02:36:38 +0530

    Force rebuild fix7

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -17,18 +17,28 @@ export class Register {
   password = '';
   name = '';
 
+  isSubmitting = false;
+
   register() {
+    if (this.isSubmitting) return; // 🔥 prevent duplicate
+    this.isSubmitting = true;
+
     this.authService.register({
       name: this.name,
       email: this.email,
       password: this.password,
-      roleId: 2 // normal user
+      roleId: 2
     }).subscribe({
       next: () => {
         alert('Registered successfully!');
         this.router.navigate(['/login']);
       },
-      error: () => alert('Registration failed')
+      error: () => {
+        alert('Registration failed');
+      },
+      complete: () => {
+        this.isSubmitting = false;
+      }
     });
   }
 
